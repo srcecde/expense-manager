b59c55c58d57c7a8320c945344916288a3ba4363
chiragr83@gmail.com <chiragr83@gmail.com>
2017-03-26 17:34:13 +0530

    Added Files

---
+++ app.py
@@ -0,0 +1,272 @@
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from wtforms import Form, TextField, validators, IntegerField, PasswordField, BooleanField, SelectField
+from functools import wraps
+from flask_sqlalchemy import SQLAlchemy
+import collections
+from passlib.hash import sha256_crypt
+import datetime
+import pygal
+import model as m
+import gc
+
+app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'memchached'
+app.config['SECRET_KEY'] = 'super secret key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/expmanager'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+
+@app.errorhandler(405)
+def method_not_found(e):
+    return render_template("405.html")
+
+
+@app.route('/')
+def main():
+    m.db.create_all()
+    return render_template('main.html')
+
+
+@app.route('/dashboard/', methods=['GET', 'POST'])
+def dashboard():
+    try:
+        if session['logged_in']:
+
+            uid = getuid()
+            gdata = m.db.session.query(m.Expense).filter_by(uid_fk=uid)
+            a = gdata.all()
+            x = []
+            y = []
+
+            for i in a:
+                x.append(i.__dict__['date'])
+
+            for i in x:
+                y.append(i.strftime("%B-%Y"))
+
+            if request.method == 'POST':
+                catmon = request.form['monselect']
+
+            gcategory = m.db.session.query(m.Expense).join(m.users, m.Category).add_columns(m.Category.categories, m.Expense.date, m.Expense.description, m.Expense.amount).filter(m.users.username==session['username'], m.Expense.uid_fk==m.users.uid, m.Expense.cid_fk==m.Category.cid).order_by(m.Expense.date)
+
+            ides = collections.defaultdict(list)
+            iamount = collections.defaultdict(list)
+            try:
+                fmon = catmon.split("-")
+            except:
+                pass
+
+            for i in gcategory:
+                xx = i.date
+                try:
+                    if fmon[0] == xx.strftime("%B") and fmon[1] == xx.strftime("%Y"):
+
+                        ides[i.categories].append(i.description)
+                        iamount[i.categories].append(i.amount)
+                except:
+                    ides[i.categories].append(i.description)
+                    iamount[i.categories].append(i.amount)
+
+            samt = {k: sum(map(int, v)) for k, v in iamount.items()}
+
+            for key in ides.keys() & samt.keys():
+                ides[key].append(samt[key])
+
+            graph = pygal.Pie()
+            graph.title = "Expense Manager"
+            # print(ides.values())
+
+            for i, j in ides.items():
+
+                graph.add(i, [{'value': j[-1], 'label': '\n,'.join(j[:-1])}])
+
+            graph_data = graph.render_data_uri()
+            gc.collect()
+
+            return render_template('dashboard.html', graph_data=graph_data, y=set(y))
+        else:
+            return redirect(url_for('login'))
+    except:
+        return redirect(url_for('login'))
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash("Login Required")
+            return redirect(url_for('login'))
+    return wrap
+
+
+@app.route('/expense/', methods=['GET', 'POST'])
+def addexp():
+
+    try:
+        if session['logged_in']:
+            uid = getuid()
+            # print(uid)
+            s = m.db.session.query(m.Category).filter_by(uid_fk=uid)
+            a = s.all()
+            x = []
+            for i in a:
+                x.append(i.__dict__['categories'])
+
+        if request.method == "POST":
+            amount = request.form['eamount']
+            category = request.form['catselect']
+            date = request.form['edate']
+            description = request.form['edescription']
+
+            user_id = getuid()
+            cat_id = m.db.session.query(m.Category).filter_by(categories=category).first()
+            eid = m.db.session.query(m.Expense.eid)
+            e_uid = eid.all()
+            e_uid = len(e_uid) + 1
+
+            expense_add = m.Expense(e_uid, user_id,  cat_id.__dict__['cid'], date, amount, description)
+            m.db.session.add(expense_add)
+            m.db.session.commit()
+            gc.collect()
+
+        return render_template('expense.html', x=x)
+
+    except:
+
+        return redirect(url_for('login'))
+
+
+@app.route('/logout/')
+@login_required
+def logout():
+    session.clear()
+    flash("Logged Out")
+    return redirect(url_for('main'))
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    error = ''
+
+    if session.get('logged_in'):
+        return redirect(url_for('dashboard'))
+    else:
+
+        try:
+            if request.method == 'POST':
+                registered_user = m.db.session.query(m.users).filter_by(username=request.form['username']).first()
+
+                if sha256_crypt.verify(request.form['password'],  registered_user.password):
+                    session['logged_in'] = True
+                    session['username'] = request.form['username']
+                    flash("Welcome! Please add Budget and Category first from Configure Tab.")
+                    return redirect(url_for('dashboard'))
+                else:
+                    error = "Invalid Credentials"
+                gc.collect()
+            return render_template('login.html', error=error)
+
+        except Exception:
+            error = "Invalid"
+            return render_template('login.html', error=error)
+
+
+def getuid():
+    s = m.db.session.query(m.users).filter_by(username=session['username']).first()
+    return s.__dict__['uid']
+
+
+@app.route('/configure/', methods=["GET", "POST"])
+def config():
+
+    try:
+        if session['logged_in']:
+
+            if request.method == 'POST' and request.form['budget']:
+                budget = request.form['budget']
+                dt = datetime.datetime.now().date()
+
+                user_id = getuid()
+
+                bid = m.db.session.query(m.Budget.bid)
+                b_uid = bid.all()
+                b_uid = len(b_uid) + 1
+
+                budget_add = m.Budget(b_uid, user_id, dt, budget)
+                m.db.session.add(budget_add)
+                m.db.session.commit()
+                gc.collect()
+                return redirect(url_for('config'))
+
+            if request.method == 'POST' and request.form['category']:
+                category = request.form['category']
+
+                user_id = getuid()
+
+                cid = m.db.session.query(m.Category.cid)
+                c_uid = cid.all()
+                c_uid = len(c_uid) + 1
+
+                category_add = m.Category(c_uid, user_id, category)
+                m.db.session.add(category_add)
+                m.db.session.commit()
+                gc.collect()
+                return redirect(url_for('config'))
+
+            return render_template('configure.html')
+        else:
+            return redirect(url_for('login'))
+    except:
+        return redirect(url_for('login'))
+
+
+@app.route('/expense/', methods=["GET", "POST"])
+def expense():
+    return render_template('expense.html')
+
+
+@app.route('/register/', methods=["GET", "POST"])
+def register():
+
+    if session.get('logged_in'):
+        return redirect(url_for('dashboard'))
+
+    else:
+        try:
+
+            if request.method == 'POST':
+
+                username = request.form['username']
+
+                email = request.form['email']
+                password = sha256_crypt.encrypt((str(request.form['password'])))
+                x = m.users.query.filter_by(username=username).all()
+
+                if len(x) > 0:
+                    flash("Username taken")
+                    return render_template('register.html')
+                else:
+                    uid = m.db.session.query(m.users.uid)
+                    i_uid = uid.all()
+                    i_uid = len(i_uid) + 1
+
+                    user = m.users(i_uid, username, password, email)
+                    m.db.session.add(user)
+                    m.db.session.commit()
+                    flash("Done")
+                    return redirect(url_for('dashboard'))
+
+            return render_template('register.html')
+        except Exception as e:
+            return(str(e))
+
+if __name__ == '__main__':
+    app.run()
---
+++ model.py
@@ -0,0 +1,94 @@
+from flask import Flask
+from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+engine = create_engine('postgresql://postgres:root@localhost:5432/', convert_unicode=True)
+
+conn = engine.connect()
+conn.connection.connection.set_isolation_level(0)
+
+try:
+    conn.execute('create database expmanager')
+except:
+    pass
+conn.connection.connection.set_isolation_level(1)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost:5432/expmanager'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class users(db.Model):
+    __tablename__ = 'users'
+    uid = db.Column('uid', db.Integer, primary_key=True)
+    username = db.Column('username', db.String(20))
+    password = db.Column('password', db.String(255))
+    email = db.Column('email', db.String(50))
+
+    def __init__(self, uid, username, password, email):
+        self.uid = uid
+        self.username = username
+        self.password = password
+        self.email = email
+
+
+class Category(db.Model):
+    __tablename__ = 'category'
+    cid = db.Column('cid', db.Integer, primary_key=True)
+    uid_fk = db.Column('uid_fk', db.Integer, db.ForeignKey(users.uid))
+    categories = db.Column('categories', db.String(100))
+
+    def __init__(self, cid, uid_fk, categories):
+        self.cid = cid
+        self.uid_fk = uid_fk
+        self.categories = categories
+
+
+class Budget(db.Model):
+    __tablename__ = 'budget'
+    bid = db.Column('bid', db.Integer, primary_key=True)
+    uid_fk = db.Column('uid_fk', db.Integer, db.ForeignKey('users.uid'))
+    bdate = db.Column('bdate', db.Date)
+    monthly_budget = db.Column('monthly_budget', db.Integer)
+
+    def __init__(self, bid, uid_fk, bdate, monthly_budget):
+        self.bid = bid
+        self.uid_fk = uid_fk
+        self.bdate = bdate
+        self.monthly_budget = monthly_budget
+
+
+class Expense(db.Model):
+    __tablename__ = 'expense'
+    eid = db.Column('eid', db.Integer, primary_key=True)
+    uid_fk = db.Column('uid_fk', db.Integer, db.ForeignKey('users.uid'))
+    cid_fk = db.Column('cid_fk', db.Integer, db.ForeignKey('category.cid'))
+    date = db.Column('date', db.Date)
+    amount = db.Column('amount', db.Integer)
+    description = db.Column('description', db.String(200))
+
+
+    def __init__(self, eid, uid_fk, cid_fk, date, amount, description):
+        self.eid = eid
+        self.uid_fk = uid_fk
+        self.cid_fk = cid_fk
+        self.date = date
+        self.amount = amount
+        self.description = description
+
+
+class Box12(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.Date, unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
